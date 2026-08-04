283513c6ad1a94e588dffdc6a1f250b28ad45a99
Jean Brazeau <jeanbrazeau@icloud.com>
2026-08-03 17:48:30 -0700

    Release 0.94

--- downtown-solutions_firmware/version.h
+++ downtown-solutions_firmware/version.h
@@ -15,7 +15,7 @@
 #ifndef NAVA_VERSION_H
 #define NAVA_VERSION_H
 
-#define FIRMWARE_VERSION "0.93"
+#define FIRMWARE_VERSION "0.94"
 
 // The splash line is " solutions " + the version, on a 16-column display.
 // A version long enough to overflow would silently lose its tail into
